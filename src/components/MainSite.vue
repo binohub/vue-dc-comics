ecfdd1a9483bd64ba7cdc8df2f7373a172926742
binohub <yeebino001@gmail.com>
2022-07-18 17:31:43 +0200

    cd - card - update

--- src/components/MainSite.vue
+++ src/components/MainSite.vue
@@ -20,7 +20,36 @@
               </h3>
             </div>
           </div>
-
+            <div class="card-container">
+            <div class="image-container">
+              <img class="img-fluid" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            </div>
+            <div class="title-container">
+              <h3>
+                {{ "American Vampire 1976" }}
+              </h3>
+            </div>
+          </div>
+            <div class="card-container">
+            <div class="image-container">
+              <img class="img-fluid" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            </div>
+            <div class="title-container">
+              <h3>
+                {{ "American Vampire 1976" }}
+              </h3>
+            </div>
+          </div>
+            <div class="card-container">
+            <div class="image-container">
+              <img class="img-fluid" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            </div>
+            <div class="title-container">
+              <h3>
+                {{ "American Vampire 1976" }}
+              </h3>
+            </div>
+          </div>
 
 
 
@@ -216,7 +245,7 @@ export default {
 #divContentDown div.image-container img {
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
   object-position: top;
 }
 #divContentDown div.title-container {
